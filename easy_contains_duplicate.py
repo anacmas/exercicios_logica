ace76cd4d4d5ecdc000444008cd574ba2164f7dc
anasilva <anacmas@gmail.com>
2023-01-10 11:42:52 -0300

    resolution contains duplicate

---
+++ easy_contains_duplicate.py
@@ -0,0 +1,18 @@
+# Problema proposto por leetcode.com
+# Link do problema: https://leetcode.com/problems/contains-duplicate/
+
+# Enunciado:
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+nums = [1, 2, 3, 1]
+sum = 0
+
+for i in range(len(nums)):
+    for j in range(i+1, len(nums)):
+        if nums[i] == nums[j]:
+            sum += 1
+
+if sum > 0:
+    print(True)
+else:
+    print(False)
