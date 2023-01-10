873833b0650dbc479a0a98dc86f020c66056240b
anasilva <anacmas@gmail.com>
2023-01-10 15:11:20 -0300

    resolução power of two

---
+++ easy_power_of_two.py
@@ -0,0 +1,23 @@
+"""Problema proposto por leetcode.com
+
+Link do problema: https://leetcode.com/problems/power-of-two/
+
+Enunciado:
+Given an integer n, return true if it is a power of two. 
+Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x
+such that n == 2^x.
+
+Dificuldade: easy"""
+
+n = 0
+
+def power_of_two(n):
+
+    for x in range(0, 50):
+        if 2**x == n:
+            return print(True)
+    return print(False)
+
+power_of_two(n)
