4f96bfcba03bf677d54163322997c89bcd96e10f
anasilva <anacmas@gmail.com>
2023-01-12 13:48:04 -0300

    resolution single number III

---
+++ medium/mediumSingleNumberIII.py
@@ -0,0 +1,39 @@
+"""Problema proposto por leetcode.com
+
+Link do problema: https://leetcode.com/problems/single-number-iii/
+
+Enunciado:
+Given an integer array nums, in which exactly two elements appear
+only once and all the other elements appear exactly twice.
+Find the two elements that appear only once. You can return the answer
+in any order.
+
+You must write an algorithm that runs in linear runtime complexity
+and uses only constant extra space.
+
+Dificuldade: medium"""
+
+nums = [1, 2, 1, 3, 2, 5]
+
+def is_single(number_array):
+
+    repeated_array = []
+    array_without_repetition = []
+    repeated = set()
+
+    for number in number_array:
+        if number not in repeated_array:
+            repeated_array.append(number)
+        else: 
+            repeated.add(number)
+
+    for item in number_array:
+        if item not in repeated:
+            array_without_repetition.append(item)
+
+
+
+    return print(array_without_repetition)
+
+
+is_single(nums)
